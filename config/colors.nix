5c70fa5968cd056aeae899ff2d3b97005190dbf0
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2024-10-18 21:10:07 +0200

    update theme

--- config/colors.nix
+++ config/colors.nix
@@ -1,6 +1,5 @@
 {
   config = {
-
     highlight = {
       RainbowRed.fg = "#E06C75";
       RainbowYellow.fg = "#E5C07B";
@@ -17,10 +16,38 @@
         flavour = "mocha";
         transparent_background = true;
         show_end_of_buffer = true;
+
         integrations = {
+          cmp = true;
+          gitsigns = true;
           neotree = true;
-          indent_blankline.enabled = true;
+          # which_key = true;
+          illuminate = {
+            enabled = true;
+            lsp = true;
+          };
           treesitter = true;
+          telescope.enabled = true;
+          indent_blankline.enabled = true;
+          native_lsp = {
+            enabled = true;
+            inlay_hints = {
+              background = true;
+            };
+            virtual_text = {
+              errors = ["italic"];
+              hints = ["italic"];
+              information = ["italic"];
+              warnings = ["italic"];
+              ok = ["italic"];
+            };
+            underlines = {
+              errors = ["underline"];
+              hints = ["underline"];
+              information = ["underline"];
+              warnings = ["underline"];
+            };
+          };
         };
       };
     };
