348d89672e38e36beb208b68a45dbd4e9ee1c65b
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2024-10-25 22:58:13 +0200

    add lsp/cmp config

--- config/cmp.nix
+++ config/cmp.nix
@@ -1,5 +1,6 @@
 {
   plugins = {
+    lspkind.enable = true;
     cmp = {
       enable = true;
       autoEnableSources = true;
@@ -9,13 +10,79 @@
             require("luasnip").lsp_expand(args.body)
           end
         '';
-        mapping.__raw = /* lua */ ''
-          cmp.mapping.preset.insert({
-            ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-            ["<CR>"] = cmp.mapping.confirm(),
-            ["<C-Space>"] = cmp.mapping.complete(),
-          })
-        '';
+
+        mapping = {
+            "<Up>" = /* lua */ "cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select})";
+            "<Down>" = /* lua */ "cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select})";
+
+            "<C-p>" = /* lua */ "cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select})";
+            "<C-n>" = /* lua */ "cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select})";
+
+            "<C-u>" = /* lua */ "cmp.mapping.scroll_docs(-4)";
+            "<C-d>" = /* lua */ "cmp.mapping.scroll_docs(4)";
+
+            "<C-e>" = /* lua */ "cmp.mapping.abort()";
+            "<C-y>" = /* lua */ "cmp.mapping.confirm({select = true})";
+            "<CR>" = /* lua */ "cmp.mapping.confirm({select = false})";
+
+            "<C-f>" = /* lua */ ''
+              cmp.mapping(
+                function(fallback)
+                  local luasnip = require 'luasnip'
+                  if luasnip.jumpable(1) then
+                    luasnip.jump(1)
+                  else
+                    fallback()
+                  end
+                end,
+                { "i", "s" }
+              )
+            '';
+
+            "<C-b>" = /* lua */ ''
+              cmp.mapping(
+                function(fallback)
+                  local luasnip = require 'luasnip'
+                  if luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                  else
+                    fallback()
+                  end
+                end,
+                { "i", "s" }
+              )
+            '';
+
+            "<Tab>" = /* lua */ ''
+              cmp.mapping(
+                function(fallback)
+                  local col = vim.fn.col('.') - 1
+
+                  if cmp.visible() then
+                    cmp.select_next_item(select_opts)
+                  elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                    fallback()
+                  else
+                    cmp.complete()
+                  end
+                end,
+                { "i", "s" }
+              )
+            '';
+
+            "<S-Tab>" = /* lua */ ''
+              cmp.mapping(
+                function(fallback)
+                  if cmp.visible() then
+                    cmp.select_prev_item(select_opts)
+                  else
+                    fallback()
+                  end
+                end,
+                { "i", "s" }
+              )
+            '';
+        };
 
         sources = [
           {
--- config/lsp.nix
+++ config/lsp.nix
@@ -1,11 +1,14 @@
 {
   # plugins.lsp-format.enable = true;
+  plugins.lint.enable = true;
   plugins.lsp = {
     enable = true;
 
     # onAttach = /* lua */ ''
     #   lsp.default_keymaps({buffer = bufnr})
     # '';
+
+    inlayHints = true;
     
     servers = {
       rust_analyzer = {
@@ -17,10 +20,21 @@
       clangd.extraOptions.switchSourceHeader = 1;
       lua_ls.enable = true;
       nil_ls.enable = true;
-      pyright.enable = true;
       bashls.enable = true;
       marksman.enable = true;
       dockerls.enable = true;
+
+      pylsp = {
+        enable = true;
+        settings.plugins = {
+          flake8.enabled = true;
+          jedi.enabled = true;
+          pycodestyle.enabled = true;
+          pyflakes.enabled = true;
+          pylint.enabled = true;
+          yapf.enabled = true;
+        };
+      };
       
       ts_ls.enable = true; # delete later
     };
