08664011d77a9e25d4e9598db1402bc93095d285
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2024-10-27 23:38:08 +0100

    add treesitter textobjects

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -21,4 +21,66 @@
     nixvimInjections = true;
   };
   plugins.treesitter-context.enable = true;
+  plugins.treesitter-textobjects = {
+    enable = true;
+    select = {
+      enable = true;
+      lookahead = true;
+      keymaps = {
+        "aa" = "@parameter.outer";
+        "ia" = "@parameter.inner";
+        "af" = "@function.outer";
+        "if" = "@function.inner";
+        "ac" = "@class.outer";
+        "ic" = "@class.inner";
+        "ii" = "@conditional.inner";
+        "ai" = "@conditional.outer";
+        "il" = "@loop.inner";
+        "al" = "@loop.outer";
+        "at" = "@comment.outer";
+      };
+    };
+    move = {
+      enable = true;
+      gotoNextStart = {
+        "]]" = "@function.outer";
+        "]m" = "@class.outer";
+      };
+      gotoNextEnd = {
+        "][" = "@function.outer";
+        "]M" = "@class.outer";
+      };
+      gotoPreviousStart = {
+        "[[" = "@function.outer";
+        "[m" = "@class.outer";
+      };
+      gotoPreviousEnd = {
+        "[]" = "@function.outer";
+        "[M" = "@class.outer";
+      };
+    };
+    swap = {
+      enable = true;
+      swapNext = {
+        "<leader>a" = "@parameters.inner";
+      };
+      swapPrevious = {
+        "<leader>A" = "@parameter.outer";
+      };
+    };
+    lspInterop = {
+      enable = true;
+      border = "single";
+      peekDefinitionCode = {
+        "<leader>df" = {
+          query = "@function.outer";
+          desc = "Peek definition outer function";
+        };
+        "<leader>dF" = {
+          query = "@class.outer";
+          desc = "Peek definition outer class";
+        };
+      };
+    };
+  };
 }
