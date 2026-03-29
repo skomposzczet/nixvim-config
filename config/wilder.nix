4d664288a6cec722cf32fefb4e96ae62d820cc96
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2026-03-29 22:08:55 +0200

    wilder...

--- config/wilder.nix
+++ config/wilder.nix
@@ -1,6 +1,22 @@
+{ lib, ... }:
 {
   plugins.wilder = {
     enable = true;
+    # TODO winborder - goofy double border makes it unusable if not palette
+    options = {
+      renderer = lib.nixvim.mkRaw ''
+        wilder.popupmenu_renderer(
+          wilder.popupmenu_palette_theme({
+            highlighter = wilder.basic_highlighter(),
+            border = 'rounded',
+            max_height = '75%',
+            min_height = 0,
+            prompt_position = 'top',
+            reverse = 0,
+          })
+        )
+      '';
+    };
     settings.modes = [ ":" "/" "?" ];
   };
 }
