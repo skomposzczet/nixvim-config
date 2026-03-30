56d65754531af707e8e5cada37a6210299c4068f
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2026-03-30 22:23:32 +0200

    fix wilder

--- config/wilder.nix
+++ config/wilder.nix
@@ -1,22 +1,20 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 {
   plugins.wilder = {
     enable = true;
-    # TODO winborder - goofy double border makes it unusable if not palette
+    # TODO make it cleaner maybe
+    package = pkgs.vimPlugins.wilder-nvim.overrideAttrs (old: {
+      patches = (old.patches or []) ++ [
+        ./patches/border.patch
+      ];
+    });
+    settings.modes = [ ":" "/" "?" ];
     options = {
       renderer = lib.nixvim.mkRaw ''
-        wilder.popupmenu_renderer(
-          wilder.popupmenu_palette_theme({
-            highlighter = wilder.basic_highlighter(),
-            border = 'rounded',
-            max_height = '75%',
-            min_height = 0,
-            prompt_position = 'top',
-            reverse = 0,
-          })
-        )
+        wilder.popupmenu_renderer({
+          highlighter = wilder.basic_highlighter(),
+        })
       '';
     };
-    settings.modes = [ ":" "/" "?" ];
   };
 }
